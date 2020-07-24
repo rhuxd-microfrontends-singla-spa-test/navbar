d59de6e6f3c1082b23d7a2b1237f731da4653711
Christie Molloy <cmolloy@redhat.com>
2020-07-24 13:43:09 -0400

    update navigation

--- src/root.component.js
+++ src/root.component.js
@@ -13,9 +13,14 @@ import {
 export default function Root(props) {
 
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isActiveItem, setIsActiveItem] = useState(0);
 
   const onNavToggle = () => {
     setIsNavOpen(!isNavOpen);
+  };
+
+  const onSelect = result => {
+    setIsActiveItem(result.itemId);
   };
 
   const logoProps = {
@@ -31,66 +36,29 @@ export default function Root(props) {
       headerTools={<PageHeaderTools>header-tools</PageHeaderTools>}
       showNavToggle
       isNavOpen={isNavOpen}
-      onNavToggle={this.onNavToggle}
+      onNavToggle={onNavToggle}
     />
   );
   
   const Sidebar = <PageSidebar nav={PageNav} isNavOpen={isNavOpen} />;
 
   const PageNav = (
-    <Nav onSelect={this.onNavSelect} aria-label="Nav">
-      {links.map((link) => {
+    <Nav onSelect={onSelect} aria-label="Nav">
+      {links.map((link, i) => {
         return (
-          <NavItem itemId={} key={link.href} to={link.href}>
-            {link.name}
+          <NavItem itemId={i} isActive={isActiveItem === i}>
+            <Link key={link.href} to={link.href}>
+              {link.name}
+            </Link>
           </NavItem>
         );
       })}
-      <NavList>
-        <NavItem itemId={0} isActive={activeItem === 0}>
-          Apicurio Studio
-        </NavItem>
-        <NavItem itemId={1} isActive={activeItem === 1}>
-          Syndesis
-        </NavItem>
-        <NavItem itemId={2} isActive={activeItem === 2}>
-          Authentication
-        </NavItem>
-        <NavItem itemId={3} isActive={activeItem === 3}>
-          Network Services
-        </NavItem>
-        <NavItem itemId={4} isActive={activeItem === 4}>
-          Server
-        </NavItem>
-      </NavList>
     </Nav>
   );
 
   return (
     <Page header={Header} sidebar={Sidebar}>
       <PageSection variant={PageSectionVariants.darker}>Section with darker background</PageSection>
-      <PageSection variant={PageSectionVariants.dark}>Section with dark background</PageSection>
-      <PageSection variant={PageSectionVariants.light}>Section with light background</PageSection>
     </Page>
-
-    <div className="h-16 flex items-center justify-between px-6 bg-primary text-white">
-      <div className="flex items-center justify-between">
-        {links.map((link) => {
-          return (
-            <Link key={link.href} className="p-6" to={link.href}>
-              {link.name}
-            </Link>
-          );
-        })}
-      </div>
-      <div className="flex items-center justify-between">
-        <a
-          href="https://github.com/react-microfrontends"
-          className="externalLink"
-        >
-          Github project
-        </a>
-      </div>
-    </div>
   );
 }
