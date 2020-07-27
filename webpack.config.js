320a08e5393258c47ec47b30ded9f024acd73aea
Christie Molloy <cmolloy@redhat.com>
2020-07-27 14:09:12 -0400

    updates to webpack to build nav

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpackMerge = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react");
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = (webpackConfigEnv) => {
   const defaultConfig = singleSpaDefaults({
@@ -11,5 +12,41 @@ module.exports = (webpackConfigEnv) => {
 
   return webpackMerge.smart(defaultConfig, {
     // customizations can go here
+    plugins: [
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      })
+    ],
+    module: {
+      rules: [{
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }, {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader?limit=100000' 
+      }, {
+        test: /\.scss$/,
+        use: [
+            // only use the mode of development
+           MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader"
+        ]
+    }, {
+    test: /\.(png|jpg|gif)$/i,
+    use: [
+      {
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+        },
+      },
+    ],
+  }]
+    }
   });
 };
